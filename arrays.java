0042d57248dafe79c83fabc247e6bfcd535d3496
Abhinaya-ponnam <ponnamabhi64@gmail.com>
2025-01-28 18:29:48 +0530

    Add files via upload

---
+++ arrays.java
@@ -0,0 +1,10 @@
+public class arrays {
+    public static void main(String[] args){
+        int[] array ={1,2,3,4,5};
+        System.out.println("the length of the array is "+array.length);
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    } 
+    
+}
